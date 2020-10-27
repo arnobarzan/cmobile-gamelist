4baf0f827aa34a8bfe3cf8b97509831833d7d8dc
Arno Barzan <arno.barzan@pxl.be>
2020-10-27 11:55:43 +0100

    Added pictures for the games

--- VideoGameListApp/VideoGameListApp/GameListPage.xaml.cs
+++ VideoGameListApp/VideoGameListApp/GameListPage.xaml.cs
@@ -15,8 +15,8 @@ namespace VideoGameListApp
         private List<VideoGame> listInCSharp = new List<VideoGame>
         {
             new VideoGame {Title="GTA5", Genre="Action", Rating=95, ReleaseDate=new DateTime(2013,01,01), CoverPictureURL="https://gamegasm.files.wordpress.com/2013/04/actual_1364906194.jpg"},
-            new VideoGame {Title="Fortnite", Genre="Shooter", Rating=85, ReleaseDate=new DateTime(2017,01,01), CoverPictureURL="https://gamegasm.files.wordpress.com/2013/04/actual_1364906194.jpg"},
-            new VideoGame {Title="Skyrim", Genre="RPG", Rating=98, ReleaseDate=new DateTime(2011,01,01), CoverPictureURL="https://gamegasm.files.wordpress.com/2013/04/actual_1364906194.jpg"}
+            new VideoGame {Title="Fortnite", Genre="Shooter", Rating=8, ReleaseDate=new DateTime(2017,01,01), CoverPictureURL="https://www.gamevideos.nl/storage/1491/fortnite.jpg"},
+            new VideoGame {Title="Skyrim", Genre="RPG", Rating=65, ReleaseDate=new DateTime(2011,01,01), CoverPictureURL="https://res.cloudinary.com/www-fun-be/image/fetch/q_auto,f_auto/https://media.fun.be/media/catalog/product/cache/43/image/768x/9df78eab33525d08d6e5fb8d27136e95/2/0/20421455.jpg"}
         };
 
         public GameListPage()
